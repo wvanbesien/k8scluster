6317a15398e432fa3f4404674a8b943ea0176f64
Ward van Besien <15829887+wvanbesien@users.noreply.github.com>
2025-11-08 13:44:54 +1100

    Update 00_deploy_prereqs.sh

--- 00_deploy_prereqs.sh
+++ 00_deploy_prereqs.sh
@@ -19,6 +19,8 @@ cat <<'EOF' | sudo tee /etc/sysctl.d/99-kubernetes-cri.conf >/dev/null
 net.bridge.bridge-nf-call-iptables  = 1
 net.bridge.bridge-nf-call-ip6tables = 1
 net.ipv4.ip_forward                 = 1
+net.ipv4.ip_nonlocal_bind           = 1
+
 EOF
 
 sudo sysctl --system
